a6c0de93af5909ca0239e4f03b318fc544d901e7
decent-scraper-1450 <jaimealba1574@gmail.com>
2020-04-23 15:24:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,13 @@
-self.class.all << self
+class Song
+  attr_accessor :name, :artist_name
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def save
+    self.class.all << self
   end
 
   def self.create
